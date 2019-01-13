11c9cd69755671570262404b753a6c618d6ebb50
Nurio Noroty <xxnurioxx@gmail.com>
2019-01-13 02:28:00 +0100

    Update base.js

--- gov.cdecountry.es/assets/js/base.js
+++ gov.cdecountry.es/assets/js/base.js
@@ -176,7 +176,7 @@ class DynamicSite {
             let paramTitle = document.querySelector("param[name='page-title']");
             let title = (paramTitle) ? paramTitle.value : "CdeCountry";
             let nUrl = url.replace("https://new.cdecountry.es/dynamic", "https://new.cdecountry.es");
-            window.history.pushState(html, title, nUrl);
+            window.history.pushState(html.innerHTML, title, nUrl);
 
             document.getElementsByTagName("main")[0].innerHTML = html.getElementsByTagName("main")[0].innerHTML;
         });
