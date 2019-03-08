037ef348d9ef386ebfe82a59e658b0cc905c2b36
Nurio Noroty <xxnurioxx@gmail.com>
2019-03-08 21:35:28 +0100

    08.03.2019.01
    + Added TV
    + Started Mail recover

---
+++ gov.cdecountry.es/sites/tv.php
@@ -0,0 +1,9 @@
+<?php 
+/************************ Variable area ************************/
+$head['title'] = "TV";
+$site['active-tab'] = "tab-tv";
+?>
+<main class="container">
+    <iframe id="tvframe" src="https://new.cdecountry.es/frames/TVFrame/" class="nofullScreen"></iframe>
+    <button class="btn btn-secondary btn-block my-2 my-sm-0 btn-lg" onclick="makeFullScreenTV()">Fullscreen</button>
+</main>
