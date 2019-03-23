a93c553f5c94f7ca34a8f20b380e98fa11675ea2
Nurio Noroty <xxnurioxx@gmail.com>
2019-03-23 09:51:36 +0100

    Update test.php

--- api.cdecountry.es/src/test.php
+++ api.cdecountry.es/src/test.php
@@ -6,7 +6,7 @@ require 'libs/PHPMailer/mailer.php';
 $to = "xxzakkenxx@gmail.com";
 $subject = "This is a test message, don't panic.";
 $html = "Inside this string i can use html tags. <b>jejejejjejeje</b>";
-$content = "Minium message here";
+$content = file_get_contents("https://new.cdecountry.es/assets/mails/password-restore.html");
 
 $mail = new Mailer($to, $subject, $html, $content);
 
