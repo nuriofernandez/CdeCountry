66eeb2f42306e9c892690f0356bf96dd9fbb1dd3
Nurio Noroty <xxnurioxx@gmail.com>
2019-04-21 14:20:38 +0200

    Map+

--- gov.cdecountry.es/sites/mapa.php
+++ gov.cdecountry.es/sites/mapa.php
@@ -3,7 +3,4 @@
 $head['title'] = "Mapa";
 $site['active-tab'] = "tab-map";
 ?>
-<main class="container">
-    <iframe id="tvframe" src="https://new.cdecountry.es/frames/Mapa/" class="nofullScreen"></iframe>
-    <button class="btn btn-secondary btn-block my-2 my-sm-0 btn-lg" onclick="makeFullScreenTV()">Fullscreen</button>
-</main>
+<iframe id="tvframe" src="https://new.cdecountry.es/frames/Mapa/" class="nofullScreen"></iframe>
