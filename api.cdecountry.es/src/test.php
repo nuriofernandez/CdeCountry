ae885c63536f03a0ec89f1d125f66280ad132ce0
xXNurioXx <xxnurioxx@gmail.com>
2019-04-23 18:36:42 +0200

    Update test.php

--- api.cdecountry.es/src/test.php
+++ api.cdecountry.es/src/test.php
@@ -6,6 +6,9 @@ require 'libs/PHPMailer/mailer.php';
 $to = "xxzakkenxx@gmail.com";
 $subject = "This is a test message, don't panic.";
 $html = file_get_contents("https://new.cdecountry.es/assets/mails/carnet.html");
+
+$html = preg_replace('/%carnet_png%/m', "https://i.imgur.com/X3emBOJ.png", $html);
+
 $content = strip_tags($html);
 
 $mail = new Mailer($to, $subject, $html, $content);
