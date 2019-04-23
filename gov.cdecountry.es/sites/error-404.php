9269c6d9aa925dd4d6bfe10768b36dc3ac7f4cf9
xXNurioXx <xxnurioxx@gmail.com>
2019-04-23 18:55:19 +0200

    404

---
+++ gov.cdecountry.es/sites/error-404.php
@@ -0,0 +1,14 @@
+<?php 
+/************************ Variable area ************************/
+$head['title'] = "404. Página no encontrada";
+$site['active-tab'] = "none";
+?>
+<main class="container">
+    <div class="my-3 p-3 bg-white rounded box-shadow">
+        <div class="d-flex">
+            <img class="noselect mr-auto ml-auto" src="https://gov.cdecountry.es/inc/img/404.png" style="height:300px">
+        </div>
+        <h1 class="text-center">PÁGINA NO ENCONTRADA</h1>
+        <p class="lead font-500 text-center">Si lo deseas, puedes volver a la página principal haciendo <a href="https://gov.cdecountry.es/">clic aquí</a>.</p>
+    </div>
+</main>
