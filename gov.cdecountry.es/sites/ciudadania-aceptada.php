8679003e336361f41a7d91a0f56b87bdcae7815b
xXNurioXx <xxnurioxx@gmail.com>
2019-04-23 18:57:05 +0200

    dynamicDiv

--- gov.cdecountry.es/sites/ciudadania-aceptada.php
+++ gov.cdecountry.es/sites/ciudadania-aceptada.php
@@ -3,18 +3,20 @@
 $head['title'] = "¡Fue aprobado como CdeCiudadano!";
 $site['active-tab'] = "none";
 ?>
-<main class="container">
-    <div class="my-3 p-3 bg-white rounded box-shadow">
-        <div class="d-flex align-items-center p-3 my-3 text-white-50">
-            <div class="ml-auto mr-1">
-                <img class="mr-auto ml-auto" src="https://i.imgur.com/Sb6Bq58.png" alt="">
+<div id="dynamicDiv">
+    <main class="container">
+        <div class="my-3 p-3 bg-white rounded box-shadow">
+            <div class="d-flex align-items-center p-3 my-3 text-white-50">
+                <div class="ml-auto mr-1">
+                    <img class="mr-auto ml-auto" src="https://i.imgur.com/Sb6Bq58.png" alt="">
+                </div>
+                <div class="ml-5 mr-auto">
+                    <h1 class="mb-0 ">¡FUE APROBADO!</h1>
+                    <h4 class="mb-0 lh-100">Revise su correo electrónico para verificar su identidad.</h4>
+                    <small>No olvide mirar en la carpeta <b>Spam</b></small>
+                </div>
             </div>
-            <div class="ml-5 mr-auto">
-                <h1 class="mb-0 ">¡FUE APROBADO!</h1>
-                <h4 class="mb-0 lh-100">Revise su correo electrónico para verificar su identidad.</h4>
-                <small>No olvide mirar en la carpeta <b>Spam</b></small>
-            </div>
+            <p class="lead font-500 text-center">Si lo deseas, puedes volver a la pagina principal haciendo <a style="color: #4395ec" href="https://gov.cdecountry.es/">clic aquí</a>.</p>
         </div>
-        <p class="lead font-500 text-center">Si lo deseas, puedes volver a la pagina principal haciendo <a style="color: #4395ec" href="https://gov.cdecountry.es/">clic aquí</a>.</p>
-    </div>
-</main>
+    </main>
+</div>
