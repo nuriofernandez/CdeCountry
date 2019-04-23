2856c24cb90f9cd4700aad112deea9eb54d845a9
xXNurioXx <xxnurioxx@gmail.com>
2019-04-23 18:24:00 +0200

    Update test.php

--- api.cdecountry.es/src/test.php
+++ api.cdecountry.es/src/test.php
@@ -5,8 +5,8 @@ require 'libs/PHPMailer/mailer.php';
 
 $to = "xxzakkenxx@gmail.com";
 $subject = "This is a test message, don't panic.";
-$html = "Inside this string i can use html tags. <b>jejejejjejeje</b>";
-$content = file_get_contents("https://new.cdecountry.es/assets/mails/carnet.html");
+$html = file_get_contents("https://new.cdecountry.es/assets/mails/carnet.html");
+$content = strip_tags($html);
 
 $mail = new Mailer($to, $subject, $html, $content);
 
