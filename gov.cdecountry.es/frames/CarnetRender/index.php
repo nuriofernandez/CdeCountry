8afa56787142cc8c763c358e575f6fb7b734385e
xXNurioXx <xxnurioxx@gmail.com>
2019-06-07 06:34:11 +0200

    Update index.php

--- gov.cdecountry.es/frames/CarnetRender/index.php
+++ gov.cdecountry.es/frames/CarnetRender/index.php
@@ -15,7 +15,7 @@ if($cid == null) die("ERROR: CdeCarnet inexistente.");
       <script type="text/javascript" src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
       <script type="text/javascript" src="https://gov.cdecountry.es/inc/js/jquery-3.3.1.js"></script>
       <script type="text/javascript" src="https://gov.cdecountry.es/inc/js/jquery.qrcode.min.js"></script>
-      <script type="text/javascript" src="https://new.cdecountry.es/frames/CarnetRender/script.js"></script>
+      <script type="text/javascript" src="https://new.cdecountry.es/frames/CarnetRender/scripts.js"></script>
       
    </head>
    <body>
