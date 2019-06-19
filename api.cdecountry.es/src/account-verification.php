8941551ece8e16f44bb75528f02e06d41a8cee82
xXNurioXx <xxnurioxx@gmail.com>
2019-06-20 00:37:52 +0200

    Update account-verification.php

--- api.cdecountry.es/src/account-verification.php
+++ api.cdecountry.es/src/account-verification.php
@@ -19,7 +19,7 @@ $request = json_decode( $_POST['json'] , true );
 if( !(isset($request['token'])) && !(isset($request['image'])) ) die( json_encode( array( "error" => "invalid_request_params") ) );
 
 // Obtain account details
-$prepare = $nlsql->getPDO()->prepare("SELECT `id`, `carnet_png` FROM `ciudadanos` WHERE `verify_token`=:token");
+$prepare = $nlsql->getPDO()->prepare("SELECT `id`, `email`, `carnet_png` FROM `ciudadanos` WHERE `verify_token`=:token");
 $prepare->bindParam(":token", $request['token'], PDO::PARAM_STR, 32);
 $prepare->execute();
 
@@ -37,7 +37,25 @@ $prepare->bindParam(":token", $request['token'], PDO::PARAM_STR, 32);
 $prepare->bindParam(":imageid", $request['image'], PDO::PARAM_STR, 15);
 $prepare->execute();
 
+/* Email user */
+
+// Headers
+$to      = $userdata['email'];
+$subject = '¡Entrega de tu carnet de CdeCiudadano!';
+
+// Obtain html message from file
+$htmlMessage = file_get_contents("https://new.cdecountry.es/assets/mails/carnet.html");
+
+// Replace variables of the message
+$htmlMessage = preg_replace('/%carnet_png%/m', ("https://i.imgur.com/".$request['image'].".png"), $htmlMessage);
+
+// Create an a plain text version
+$textMessage = strip_tags($htmlMessage);
+
+// Send the email
+$mail = new Mailer($to, $subject, $htmlMessage, $textMessage);
+
 // Print the response
-print( json_encode( array( "token" => $request['token'], "image" => $request['image'], "identity" => $userdata['id'] ) ) );
+print( json_encode( array( "token" => $request['token'], "image" => $request['image'], "identity" => $userdata['id'], "mailed"=>'true' ) ) );
 
 ?>
